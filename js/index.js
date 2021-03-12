1e4fda0d1260074ab0a33158e752ae8bfef8b3bf
Rigalkumar Sanjaybhai Patel <regalpatel273@users.noreply.github.com>
2021-03-12 06:05:06 +0530

    javascript function

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var $ = function(id){
+    return document.getElementById(id);
+}
+var movieSelection = function(){
+    var movie = $("movies");
+    movie.value
+}
+window.onload(){
+    $("pay").sub
+}
